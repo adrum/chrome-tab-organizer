af14cc7fcff40eecac22f0e76344a87fc626ecf9
Austin Drummond <adrum0x7@gmail.com>
2020-05-12 21:06:54 -0400

    Init Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Chrome Tab Organizer
+
+Simple Chrome extension to manage tabs. Offers the follow commands:
+
+- Sort By Title
+    - Sorts all tabs in the current window by the Title of the page loaded in the tab.
+- Sort By Website
+    - Sorts all tabs in the current window by the URL of the page loaded in the tab.
+- Sort By Domain
+    - Sorts all tabs in the current window by the root domain the page loaded in the tab. This ignores all subdomains.
+- Merge All Windows
+    - Moves all tabs from other windows and appends them to the end of the current window.
+- Move Tab to New Window
+    - Creates a new window with the current tab.
+
---
+++ src/browser_action/browser_action.js
@@ -0,0 +1,83 @@
+
+
+function sortByTitle() {
+  sortBy('title')
+}
+
+function sortByWebsite() {
+  sortBy('url')
+}
+
+function sortByDomain() {
+  sortBy('domain')
+}
+
+function closePopover() {
+  window.close();
+}
+
+function getProp(obj, property) {
+  switch (property) {
+    case 'title':
+      return obj.title.toLowerCase()
+    case 'url':
+      return obj.url.toLowerCase()
+    case 'domain':
+    if (obj.url.toLowerCase().indexOf('http') > -1) {
+      var l = document.createElement("a");
+      l.href = obj.url;
+      var hostname = l.hostname;
+      const hostnameArray = hostname.split('.')
+      const numberOfSubdomains = hostnameArray.length - 2
+      return hostnameArray.length === 2 ? hostname : hostnameArray.slice(numberOfSubdomains).join('.')
+    }
+    return obj.title.toLowerCase()
+  }
+}
+
+function sortBy(property) {
+  chrome.windows.getCurrent(function(window){
+    chrome.tabs.query({windowId: window.id}, function(tabs){
+      var newOrder = tabs.sort((a, b) => (getProp(a, property) > getProp(b, property) ? 1 : -1)).map((t)=>t.id);
+      chrome.tabs.move(newOrder, {index: 0}, function(){
+        // alert('Tabs Reordered.')
+        closePopover();
+      })
+    })
+  })
+}
+
+function mergeAllWindows() {
+  chrome.windows.getCurrent(function(window){
+    chrome.tabs.query({currentWindow: false}, function(tabs){
+        if (tabs.length > 0) {
+          var newOrder = tabs.map((t)=>t.id);
+          chrome.tabs.move(newOrder, {index: -1, windowId: window.id}, function(){
+            closePopover();
+          });
+        };
+      });
+    })
+}
+
+function moveToNew() {
+  chrome.windows.getCurrent(function(window){
+    chrome.tabs.query({windowId: window.id}, function(tabs){
+      if (tabs.length < 2) return;
+      for (var i in tabs) {
+        var tab = tabs[i];
+        if (!tab.active) continue;
+        chrome.windows.create({focused: true, tabId: tab.id}, function(window){
+          closePopover();
+        });
+      }
+    })
+  })
+
+}
+
+document.getElementById("sort-domain") .addEventListener("click", sortByDomain);
+document.getElementById("sort-title") .addEventListener("click", sortByTitle);
+document.getElementById("sort-url") .addEventListener("click", sortByWebsite);
+document.getElementById("merge").addEventListener("click", mergeAllWindows);
+document.getElementById("move-new").addEventListener("click", moveToNew);
