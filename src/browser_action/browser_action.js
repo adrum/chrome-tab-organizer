a5e6e6452cb6f9e7ef618335bf79b6ceab57fd39
Austin Drummond <adrum0x7@gmail.com>
2025-06-20 13:35:42 -0400

    formatting

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,15 +1,13 @@
-
-
 function sortByTitle() {
-  sortBy('title')
+  sortBy("title");
 }
 
 function sortByWebsite() {
-  sortBy('url')
+  sortBy("url");
 }
 
 function sortByDomain() {
-  sortBy('domain')
+  sortBy("domain");
 }
 
 function closePopover() {
@@ -18,54 +16,62 @@ function closePopover() {
 
 function getProp(obj, property) {
   switch (property) {
-    case 'title':
-      return obj.title.toLowerCase()
-    case 'url':
-      return obj.url.toLowerCase()
-    case 'domain':
-    if (obj.url.toLowerCase().indexOf('http') > -1) {
-      var l = document.createElement("a");
-      l.href = obj.url;
-      var hostname = l.hostname;
-      const hostnameArray = hostname.split('.')
-      const numberOfSubdomains = hostnameArray.length - 2
-      return hostnameArray.length === 2 ? hostname : hostnameArray.slice(numberOfSubdomains).join('.')
-    }
-    return obj.title.toLowerCase()
+    case "title":
+      return obj.title.toLowerCase();
+    case "url":
+      return obj.url.toLowerCase();
+    case "domain":
+      if (obj.url.toLowerCase().indexOf("http") > -1) {
+        var l = document.createElement("a");
+        l.href = obj.url;
+        var hostname = l.hostname;
+        const hostnameArray = hostname.split(".");
+        const numberOfSubdomains = hostnameArray.length - 2;
+        return hostnameArray.length === 2
+          ? hostname
+          : hostnameArray.slice(numberOfSubdomains).join(".");
+      }
+      return obj.title.toLowerCase();
   }
 }
 
 function sortBy(property) {
-  chrome.windows.getCurrent(function(window){
-    chrome.tabs.query({windowId: window.id}, function(tabs){
-      var newOrder = tabs.sort((a, b) => (getProp(a, property) > getProp(b, property) ? 1 : -1)).map((t)=>t.id);
-      chrome.tabs.move(newOrder, {index: 0}, function(){
+  chrome.windows.getCurrent(function (window) {
+    chrome.tabs.query({ windowId: window.id }, function (tabs) {
+      var newOrder = tabs
+        .sort((a, b) => (getProp(a, property) > getProp(b, property) ? 1 : -1))
+        .map((t) => t.id);
+      chrome.tabs.move(newOrder, { index: 0 }, function () {
         // alert('Tabs Reordered.')
         closePopover();
-      })
-    })
-  })
+      });
+    });
+  });
 }
 
 function mergeAllWindows() {
-  chrome.windows.getCurrent(function(window){
-    chrome.tabs.query({currentWindow: false}, function(tabs){
-        if (tabs.length > 0) {
-          var newOrder = tabs.map((t)=>t.id);
-          chrome.tabs.move(newOrder, {index: -1, windowId: window.id}, function(){
+  chrome.windows.getCurrent(function (window) {
+    chrome.tabs.query({ currentWindow: false }, function (tabs) {
+      if (tabs.length > 0) {
+        var newOrder = tabs.map((t) => t.id);
+        chrome.tabs.move(
+          newOrder,
+          { index: -1, windowId: window.id },
+          function () {
             closePopover();
-          });
-        } else {
-          closePopover();
-        }
-      });
-    })
+          }
+        );
+      } else {
+        closePopover();
+      }
+    });
+  });
 }
 
 function moveToNew() {
-  if (typeof browser != 'undefined') {
-    browser.windows.getCurrent().then(function(window){
-      browser.tabs.query({windowId: window.id}).then(function(tabs){
+  if (typeof browser != "undefined") {
+    browser.windows.getCurrent().then(function (window) {
+      browser.tabs.query({ windowId: window.id }).then(function (tabs) {
         if (tabs.length < 2) {
           closePopover();
           return;
@@ -73,17 +79,17 @@ function moveToNew() {
         for (var i in tabs) {
           var tab = tabs[i];
           if (!tab.active) continue;
-          browser.windows.create({tabId: tab.id}).then(function(){
+          browser.windows.create({ tabId: tab.id }).then(function () {
             closePopover();
-          })
+          });
         }
-      })
-    })
+      });
+    });
     return;
   }
 
-  chrome.windows.getCurrent(function(window){
-    chrome.tabs.query({windowId: window.id}, function(tabs){
+  chrome.windows.getCurrent(function (window) {
+    chrome.tabs.query({ windowId: window.id }, function (tabs) {
       if (tabs.length < 2) {
         closePopover();
         return;
@@ -91,16 +97,19 @@ function moveToNew() {
       for (var i in tabs) {
         var tab = tabs[i];
         if (!tab.active) continue;
-        chrome.windows.create({focused: true, tabId: tab.id}, function(window){
-          closePopover();
-        });
+        chrome.windows.create(
+          { focused: true, tabId: tab.id },
+          function (window) {
+            closePopover();
+          }
+        );
       }
-    })
-  })
+    });
+  });
 }
 
-document.getElementById("sort-domain") .addEventListener("click", sortByDomain);
-document.getElementById("sort-title") .addEventListener("click", sortByTitle);
-document.getElementById("sort-url") .addEventListener("click", sortByWebsite);
+document.getElementById("sort-domain").addEventListener("click", sortByDomain);
+document.getElementById("sort-title").addEventListener("click", sortByTitle);
+document.getElementById("sort-url").addEventListener("click", sortByWebsite);
 document.getElementById("merge").addEventListener("click", mergeAllWindows);
 document.getElementById("move-new").addEventListener("click", moveToNew);
